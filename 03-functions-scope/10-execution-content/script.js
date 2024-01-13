160c36c65da35cdd834cce7c85f40a3020f48dab
Arthur Lacerda <arthur.srl@gmail.com>
2024-01-13 16:15:30 -0300

    Module 4 - `Functions scope`

---
+++ 03-functions-scope/10-execution-content/script.js
@@ -0,0 +1,12 @@
+const x = 50;
+const y = 100;
+
+function getSum(n1, n2) {
+  const sum = n1 + n2;
+  return sum;
+}
+
+const sum1 = getSum(x, y);
+const sum2 = getSum(10, 6);
+
+console.log(sum1, sum2);
